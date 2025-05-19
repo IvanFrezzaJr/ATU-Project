9ec0689b339f9c5d823c66aca8de826dcab9790f
Ivan Jose Frezza Junior <ivanfrezza@gmail.com>
2025-05-19 19:40:00 +0100

    #47: add - about page

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,5 +1,34 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+import styles from '../styles/Home.module.css'
+
+import student from '../assets/student.jpeg'
+
+
+
 const AboutPage = () => {
-  return <p>About page</p>
+  return (
+    <div>
+      <Header />
+      <main>
+        <div class="viewport">
+          <div class={styles.logo}>
+            <div>
+              <h3>
+                <strong>Ivan</strong> Frezza
+              </h3>
+              <img src={student} alt="student"  style={{ borderRadius: '50%' }} />
+            </div>
+          </div>
+          <div class="center">
+            Hello! My name is Ivan, and this website was created as part of my final project for my degree in Contemporary Software Development. It was developed for academic purposes only and is not intended for production use.
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default AboutPage
