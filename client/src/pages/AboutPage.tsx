36f679b60ad92d6e5902d01095394c5bc0b49ad3
Ivan Jose Frezza Junior <ivanfrezza@gmail.com>
2025-05-20 22:08:07 +0100

    #47: fix - eslint format

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -5,8 +5,6 @@ import styles from '../styles/Home.module.css'
 
 import student from '../assets/student.jpeg'
 
-
-
 const AboutPage = () => {
   return (
     <div>
@@ -18,11 +16,13 @@ const AboutPage = () => {
               <h3>
                 <strong>Ivan</strong> Frezza
               </h3>
-              <img src={student} alt="student"  style={{ borderRadius: '50%' }} />
+              <img src={student} alt="student" style={{ borderRadius: '50%' }} />
             </div>
           </div>
           <div class="center">
-            Hello! My name is Ivan, and this website was created as part of my final project for my degree in Contemporary Software Development. It was developed for academic purposes only and is not intended for production use.
+            Hello! My name is Ivan, and this website was created as part of my final project for my
+            degree in Contemporary Software Development. It was developed for academic purposes only
+            and is not intended for production use.
           </div>
         </div>
       </main>
